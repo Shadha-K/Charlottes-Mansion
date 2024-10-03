7f1ee7b779a9e3117453a6bb19aa5cf03f0e9a3e
Aditi <adscrabby@aol.com>
2024-10-02 21:49:09 -0500

    added a opening scene

---
+++ intro.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	$AnimationPlayer.play("fade_in")
+	await get_tree().create_timer(6).timeout
+	$AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://main menu/main_menu.tscn")
