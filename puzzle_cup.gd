ba95794e07b85edcfe006826e070253c08bb39c3
Aditi <adscrabby@aol.com>
2024-10-09 22:00:57 -0500

    added puzzle cup and tray animations

---
+++ puzzle_cup.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animated_sprite_2d.play("cup")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
