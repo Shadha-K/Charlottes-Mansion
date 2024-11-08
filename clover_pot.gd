8a36705ad766d738134038ff576ebe34bac35c51
Andrea Poklar <a.poklar@yahoo.com>
2024-11-07 23:31:57 -0600

    added glowing plant

--- clover_pot.gd
+++ clover_pot.gd
@@ -2,12 +2,14 @@ extends Area2D
 
 @onready var label: Label = $Label
 @onready var label2: Label = $Label2
-@onready var clover_plant: Sprite2D = $Sprite2D2
+@onready var glowing_plant: AnimatedSprite2D = $glowingclover
+@onready var clover: Sprite2D = $Clover
 
 func _ready():
 	label.visible = false
 	label2.visible = false
-	clover_plant.visible = false  
+	glowing_plant.visible = false 
+	clover.visible = false 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Alex" and get_tree().root.get_node("GameState").has_clover and not GameState.clover_planted: 
@@ -26,14 +28,16 @@ func _process(_delta):
 			label.visible = false
 			label2.visible = true
 			GameState.clover_planted = true
-			clover_plant.visible = true
-			get_tree().change_scene_to_file("res://glowing_clover.tscn")
-			#GlobalHotbar.item_used("Clover")
+			glowing_plant.visible = true
+			glowing_plant.play("glowingclover")
+			GlobalHotbar.item_used("Clover")
 			
-	if not GameState.has_clubs:
+	elif not GameState.has_clubs and GameState.clover_planted:
 		if Input.is_action_just_pressed("interact") and label2.visible:
 			label2.visible = false;
 			GameState.has_clubs = true
+			glowing_plant.visible = false
+			clover.visible = true
 			GlobalHotbar.add_item_to_slot("Club Card", preload("res://assets/puzzle_objects/AceOfClubs.png"))
 			
 func interact_with_pot():
