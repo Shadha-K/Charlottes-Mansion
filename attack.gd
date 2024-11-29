7c07e41f968f4427dd9834ac582964953804d9e0
Minnie Candy <ck011604@gmail.com>
2024-11-28 18:31:32 -0600

    knockback on teacups

--- attack.gd
+++ attack.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var damage: int = 10
+@export var knockback_force: float = 500
 
 #func _ready() -> void:
 	#monitoring = false
@@ -19,3 +20,6 @@ func _on_body_entered(body: Node2D) -> void:
 		if child is Damageable:
 			child.hit(damage)
 	
+	if body is TeacupEnemy:
+		var knockback_direction = (body.global_position - global_position).normalized()
+		body.apply_knockback(knockback_direction * knockback_force)
