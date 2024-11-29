cd96454cb5486c4860c1ad1f1dbfa3b135a78b0d
ameuph <amar.jazi00@gmail.com>
2024-11-29 17:48:39 -0600

    music fix dialogue

--- main_theme.gd
+++ main_theme.gd
@@ -4,6 +4,7 @@ extends Node
 var is_playing = false
 var theme_song = load("res://main menu/charlotte's mansion main theme.mp3")
 func _ready():
+	music_player.process_mode = Node.PROCESS_MODE_ALWAYS
 	if not is_playing:
 		music_player.stream = theme_song
 		add_child(music_player)
@@ -34,6 +35,7 @@ func play_new_song(song_path: String):
 	if music_player.playing:
 		stop_music()  
 	theme_song = load(song_path)  
-	music_player.stream = theme_song  
+	music_player.stream = theme_song 
+	music_player.process_mode = Node.PROCESS_MODE_ALWAYS 
 	music_player.play() 
 	is_playing = true
