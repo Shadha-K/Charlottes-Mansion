b3c4307dfc08e48a470acea6a7586cbcd47c4a6c
Andrea Poklar <a.poklar@yahoo.com>
2024-11-29 20:19:06 -0600

    added how to play button

--- main menu/main_menu.gd
+++ main menu/main_menu.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var how_to_play_page: AnimatedSprite2D = $AnimatedSprite2D2
+@onready var other_buttons: TextureRect = $TextureRect
+
+func _ready():
+	how_to_play_page.visible = false
+	other_buttons.visible = true
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/cutscene.tscn")
 	Main_Theme_Music.stop_music()
@@ -7,3 +14,11 @@ func _on_play_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_how_to_play_pressed() -> void:
+	how_to_play_page.visible = true
+	other_buttons.visible = false
+
+func _on_exit_how_to_play_pressed() -> void:
+	how_to_play_page.visible = false
+	other_buttons.visible = true
