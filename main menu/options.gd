705e9f54b1e3eec6316a474411bd3d02153c62f1
MohHi141 <mohamedhilal025@gmail.com>
2024-12-01 21:04:31 -0600

    fixed screen size

--- main menu/options.gd
+++ main menu/options.gd
@@ -12,4 +12,4 @@ func _on_fullscreen_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
