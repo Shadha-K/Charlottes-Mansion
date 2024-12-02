1d9d40f7bb3c61b727ec3d10a56b7bb921cda7f3
MohHi141 <mohamedhilal025@gmail.com>
2024-12-02 03:08:26 -0600

    Pause Menu Draft

---
+++ scenes/pause_menu.gd
@@ -0,0 +1,52 @@
+extends Control
+
+@onready var exit_how_to_play: Button = $"AnimatedSprite2D2/exit how to play"
+@onready var how_to_play_page: AnimatedSprite2D = $AnimatedSprite2D2
+@onready var other_buttons: MarginContainer = $Options/MarginContainer
+@onready var options: Control = $Options
+@onready var exit_options: Button = $Options/exit_options
+@onready var fullscreen: CheckButton = $Options/MarginContainer/VBoxContainer/Fullscreen
+@onready var check_box: CheckBox = $Options/MarginContainer/VBoxContainer/CheckBox
+@onready var volume: HSlider = $Options/MarginContainer/VBoxContainer/Volume
+
+@onready var living_room: Node2D = $"../.."
+
+func _ready():
+	how_to_play_page.visible = false
+	other_buttons.visible = true
+	options.visible = false
+
+
+func _on_resume_pressed() -> void:
+	living_room.pauseMenu()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_how_to_play_pressed() -> void:
+	how_to_play_page.visible = true
+	how_to_play_page.play("default")
+	how_to_play_page.z_index = 10  
+	other_buttons.visible = false
+
+
+func _on_exit_how_to_play_pressed() -> void:
+	how_to_play_page.visible = false
+	other_buttons.visible = true
+
+func _on_options_pressed() -> void:
+	options.visible = true
+	other_buttons.visible = false
+	other_buttons.z_index = 10  
+	print("ColorRect visibility: ", $Options/ColorRect.visible)
+	print("Volume visibility: ", volume.visible)
+	print("Fullscreen visibility: ", fullscreen.visible)
+
+
+func _on_exit_options_pressed() -> void:
+	options.visible = false
+	other_buttons.visible = true
+	# Reset visibility of child nodes if needed.
+	$Options/ColorRect.visible = true
+	volume.visible = true
+	fullscreen.visible = true
--- startdialogue.gd
+++ startdialogue.gd
@@ -1,58 +1,75 @@
 extends Node2D
 
+var paused = false
+@onready var pause_menu: Control = $UI/PauseMenu
 
 func _ready():
+	# Hide the pause menu when the game starts
+	pause_menu.hide()
 
-	var player =get_node("TileMap/Alex")
-	var charlotte= get_node("TileMap/Charlotte")
-	var spawn_point=""
-	var spawn_point2=""
-	if GameState.last_spawn_point_Alex != "" and GameState.last_spawn_point_charlotte !="" and GameState.last_scene_exited=="Bedroom":
+	var player = get_node("TileMap/Alex")
+	var charlotte = get_node("TileMap/Charlotte")
+	var spawn_point = ""
+	var spawn_point2 = ""
+
+	# Handle spawn point logic (no changes here)
+	if GameState.last_spawn_point_Alex != "" and GameState.last_spawn_point_charlotte != "" and GameState.last_scene_exited == "Bedroom":
 		spawn_point = get_node(GameState.last_spawn_point_Alex)
-		spawn_point2= get_node(GameState.last_spawn_point_charlotte)
+		spawn_point2 = get_node(GameState.last_spawn_point_charlotte)
 		if spawn_point and player and spawn_point2 and charlotte:
 			player.position = spawn_point.global_position
-			player.direction = Vector2(1,0)
+			player.direction = Vector2(1, 0)
 			player.update_animation_parameters()
-			charlotte.position=spawn_point2.global_position
-	elif GameState.KtoLR_spawn_point_Alex!="" and GameState.KtoLR_spawn_point_charlotte !="" and GameState.last_scene_exited=="Kitchen":
+			charlotte.position = spawn_point2.global_position
+	elif GameState.KtoLR_spawn_point_Alex != "" and GameState.KtoLR_spawn_point_charlotte != "" and GameState.last_scene_exited == "Kitchen":
 		spawn_point = get_node(GameState.KtoLR_spawn_point_Alex)
-		spawn_point2=get_node(GameState.KtoLR_spawn_point_charlotte)
-		if spawn_point and player and spawn_point2 and charlotte:
-			player.position =spawn_point.global_position
-			charlotte.position=spawn_point2.global_position
-	elif GameState.FireToLR_spawn_Alex !="" and GameState.FireToLR_spawn_Char !="" and GameState.last_scene_exited=="Fire":
-		spawn_point= get_node(GameState.FireToLR_spawn_Alex)
-		spawn_point2=get_node(GameState.FireToLR_spawn_Char)
+		spawn_point2 = get_node(GameState.KtoLR_spawn_point_charlotte)
 		if spawn_point and player and spawn_point2 and charlotte:
 			player.position = spawn_point.global_position
-			player.direction = Vector2(0,-1)
-			player.update_animation_parameters()
-			charlotte.position=spawn_point2.global_position
-	elif GameState.BasementToLR_spawn_Alex!=""and GameState.BasementToLR_spawn_Char!="" and GameState.last_scene_exited=="Basement":
-		spawn_point=get_node(GameState.BasementToLR_spawn_Alex)
-		spawn_point2=get_node(GameState.BasementToLR_spawn_Char)
+			charlotte.position = spawn_point2.global_position
+	elif GameState.FireToLR_spawn_Alex != "" and GameState.FireToLR_spawn_Char != "" and GameState.last_scene_exited == "Fire":
+		spawn_point = get_node(GameState.FireToLR_spawn_Alex)
+		spawn_point2 = get_node(GameState.FireToLR_spawn_Char)
 		if spawn_point and player and spawn_point2 and charlotte:
-			player.position =spawn_point.global_position
-			player.direction=Vector2(0,1)
+			player.position = spawn_point.global_position
+			player.direction = Vector2(0, -1)
 			player.update_animation_parameters()
-			charlotte.position=spawn_point2.global_position
-			
-		
-			
+			charlotte.position = spawn_point2.global_position
+	elif GameState.BasementToLR_spawn_Alex != "" and GameState.BasementToLR_spawn_Char != "" and GameState.last_scene_exited == "Basement":
+		spawn_point = get_node(GameState.BasementToLR_spawn_Alex)
+		spawn_point2 = get_node(GameState.BasementToLR_spawn_Char)
+		if spawn_point and player and spawn_point2 and charlotte:
+			player.position = spawn_point.global_position
+			player.direction = Vector2(0, 1)
+			player.update_animation_parameters()
+			charlotte.position = spawn_point2.global_position
+
+	# Opening cutscene logic
 	if GameState.opening:
 		Main_Theme_Music.pause_music()
 		Main_Theme_Music.play_new_song("res://assets/gameplay_music.mp3") 
-		var dialogue=DialogueManager.show_example_dialogue_balloon(load("res://dialogue/opening.dialogue"), "start")
+		var dialogue = DialogueManager.show_example_dialogue_balloon(load("res://dialogue/opening.dialogue"), "start")
 		GameState.opening = false
-		DialogueManager.process_mode=Node.PROCESS_MODE_ALWAYS
-		dialogue.process_mode=Node.PROCESS_MODE_ALWAYS
-		get_tree().paused=true
+		DialogueManager.process_mode = Node.PROCESS_MODE_ALWAYS
+		dialogue.process_mode = Node.PROCESS_MODE_ALWAYS
+		get_tree().paused = true
 		DialogueManager.dialogue_ended.connect(_unpause)
 		return
 
-
-func _unpause(_resource: ):
-	
-	get_tree().paused=false
+func _unpause(_resource):
+	get_tree().paused = false
 	return
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
