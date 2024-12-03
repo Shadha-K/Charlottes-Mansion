7d5455aacc4c2db05c411e17a3d7e60e508fba42
Andrea Poklar <a.poklar@yahoo.com>
2024-12-03 11:05:29 -0600

    minor changes

--- scripts/DropZones/drop_zone_spades.gd
+++ scripts/DropZones/drop_zone_spades.gd
@@ -6,9 +6,9 @@ func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	if typeof(data) == TYPE_DICTIONARY and data.has("name") and data["name"] == "AceOfSpades":
 		if not GameState.spades_card:
-			GlobalHotbar.item_used("AceOfSpades")
+			GlobalHotbar.item_used("AceofSpades")
 			GameState.spades_card = true
 			
-			var spade_card_node = get_node("../AceOfSpades")
+			var spade_card_node = get_node("../AceofSpades")
 			if spade_card_node:
 				spade_card_node.visible = true
