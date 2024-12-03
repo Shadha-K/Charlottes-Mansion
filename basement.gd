1fed44771a2e329218e262fc70442edf0ad31be2
MohHi141 <mohamedhilal025@gmail.com>
2024-12-03 15:46:56 -0600

    music

--- basement.gd
+++ basement.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var scary_atmosphere: AudioStreamPlayer2D = $scary_atmosphere
+@export var boss_music_stream: AudioStream = preload("res://assets/Shadows of Despair.mp3")
+@onready var boss_music: AudioStreamPlayer = $AudioStreamPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -8,6 +11,10 @@ func _ready() -> void:
 	GameState.opening = false
 	Main_Theme_Music.stop_music()
 	scary_atmosphere.play()
+	
+	boss_music.stream = boss_music_stream
+	boss_music.play()
+
 	DialogueManager.process_mode=Node.PROCESS_MODE_ALWAYS
 	dialogue.process_mode=Node.PROCESS_MODE_ALWAYS
 	get_tree().paused=true
